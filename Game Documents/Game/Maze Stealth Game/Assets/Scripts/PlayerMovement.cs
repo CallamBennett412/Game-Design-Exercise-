001d8cbb128f63cc4f165bcabf53778282ffb328
CallamBennett412 <33025245+CallamBennett412@users.noreply.github.com>
2018-02-28 12:51:39 +0000

    Game stuff
    Game

---
+++ Game Documents/Game/Maze Stealth Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement: MonoBehaviour
+{
+    public float speed = 5.0f;
+    Rigidbody2D rigidbody2D;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rigidbody2D.velocity = new Vector2(x, y) * speed;
+        rigidbody2D.angularVelocity = 0.0f;
+    }
+}
